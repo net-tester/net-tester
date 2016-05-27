2657b253dfd2bd476c08e60c141eb3448eee6d53
Yasuhito Takamiya <yasuhito@gmail.com>
2016-05-27 14:48:17 +0900

    0xabc -> dad1c001 (daddy cool)

--- app/flows/host_to_patch_flow.rb
+++ app/flows/host_to_patch_flow.rb
@@ -1,12 +1,12 @@
 class HostToPatchFlow < ActiveFlow::Base
   def self.create(in_port:)
-    send_flow_mod_add(0xabc,
+    send_flow_mod_add(0xdad1c001,
                       match: Match.new(in_port: in_port),
                       actions: SendOutPort.new(Host.all.size + 1))
   end
 
   def self.all
-    flow_stats(0xabc).stats.select do |each|
+    flow_stats(0xdad1c001).stats.select do |each|
       each.actions.size == 1 && each.actions.first.port == Host.all.size + 1
     end
   end
--- app/flows/vlan_host_to_patch_flow.rb
+++ app/flows/vlan_host_to_patch_flow.rb
@@ -1,6 +1,6 @@
 class VlanHostToPatchFlow < ActiveFlow::Base
   def self.create(in_port:, vlan_id:)
-    send_flow_mod_add(0xabc,
+    send_flow_mod_add(0xdad1c001,
                       match: Match.new(in_port: in_port),
                       actions: [SetVlanVid.new(vlan_id),
                                 SendOutPort.new(Host.all.size + 1)])
