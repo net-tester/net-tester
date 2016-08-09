2e799b7c47d00a7bff4be6357ed3db486ad2576a
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-09 15:12:19 +0900

    Add NetTester.create_patch method

--- lib/net_tester.rb
+++ lib/net_tester.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 require 'net_tester/command'
 require 'net_tester/test_switch'
+require 'phut'
+require 'trema'
+
+# Base module
+module NetTester
+  def self.creaate_patch(source_port:, source_mac_address:, destination_port:)
+    Trema.trema_process('NetTesterController', Phut.socket_dir).controller
+         .create_patch(source_port: source_port,
+                       source_mac_address: source_mac_address,
+                       destination_port: destination_port)
+  end
+end
