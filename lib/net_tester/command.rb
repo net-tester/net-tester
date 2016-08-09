0c3bf0ed7a08151a4d1ae9c61f7add22fb6d486b
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-09 11:49:18 +0900

    Add mac_address: parameter to Command.add_netns

--- lib/net_tester/command.rb
+++ lib/net_tester/command.rb
@@ -40,11 +40,12 @@ module NetTester
       end
     end
 
-    def self.add_netns(port_number:, name:, ip_address:, netmask:, route:)
+    def self.add_netns(port_number:, name:, ip_address:, mac_address:, netmask:, route:)
       port_name = "port#{port_number}"
       link = Phut::Link.create(name, port_name)
       netns = Phut::Netns.create(name: name,
                                  ip_address: ip_address,
+                                 mac_address: mac_address,
                                  netmask: netmask,
                                  route: route)
       @@test_switch.add_numbered_port port_number, link.device(port_name)
