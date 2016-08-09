ee3cd1de2472986c9ec45e02957dd2d4c4342c96
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-09 11:18:31 +0900

    Make NetTester::Command.add_netns return a netns

--- lib/net_tester/command.rb
+++ lib/net_tester/command.rb
@@ -43,11 +43,12 @@ module NetTester
     def self.add_netns(port_number:, name:, ip_address:, netmask:, route:)
       port_name = "port#{port_number}"
       link = Phut::Link.create(name, port_name)
-      Phut::Netns.create(name: name,
-                         ip_address: ip_address,
-                         netmask: netmask,
-                         route: route)
+      netns = Phut::Netns.create(name: name,
+                                 ip_address: ip_address,
+                                 netmask: netmask,
+                                 route: route)
       @@test_switch.add_numbered_port port_number, link.device(port_name)
+      netns
     end
 
     def self.connect_switch(device:, port_number:)
