e30071a58312d105e37bb33177bfcd51927ee803
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-09 15:15:25 +0900

    Fix typo

--- lib/net_tester.rb
+++ lib/net_tester.rb
@@ -6,7 +6,7 @@ require 'trema'
 
 # Base module
 module NetTester
-  def self.creaate_patch(source_port:, source_mac_address:, destination_port:)
+  def self.create_patch(source_port:, source_mac_address:, destination_port:)
     Trema.trema_process('NetTesterController', Phut.socket_dir).controller
          .create_patch(source_port: source_port,
                        source_mac_address: source_mac_address,
