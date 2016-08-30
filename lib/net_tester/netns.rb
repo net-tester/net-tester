f4085db001e7e9d65ee615b46ff3ecc24196b2c1
Yasuhito Takamiya <yasuhito@gmail.com>
2016-08-30 09:36:21 +0900

    Refactoring

--- lib/net_tester/netns.rb
+++ lib/net_tester/netns.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/module/delegation'
 require 'phut/netns'
 
 module NetTester
   # Phut::Netns wrapper class
   class Netns
+    delegate :exec, to: :@netns
+
     # rubocop:disable ParameterLists
-    def self.new(name:,
-                 mac_address:,
-                 ip_address:,
-                 netmask:,
-                 gateway:,
-                 virtual_port_number:,
-                 physical_port_number:)
-      netns = Phut::Netns.create(name: name,
-                                 mac_address: mac_address,
-                                 ip_address: ip_address,
-                                 netmask: netmask,
-                                 route: { net: '0.0.0.0', gateway: gateway })
-      NetTester.patch_netns_to_physical_port(netns: netns,
+    def initialize(name:,
+                   mac_address:,
+                   ip_address:,
+                   netmask:,
+                   gateway:,
+                   virtual_port_number:,
+                   physical_port_number:)
+      @netns = Phut::Netns.create(name: name,
+                                  mac_address: mac_address,
+                                  ip_address: ip_address,
+                                  netmask: netmask,
+                                  route: { net: '0.0.0.0', gateway: gateway })
+      NetTester.patch_netns_to_physical_port(netns: @netns,
                                              virtual_port_number: virtual_port_number,
                                              physical_port_number: physical_port_number)
-      netns
     end
     # rubocop:enable ParameterLists
   end
