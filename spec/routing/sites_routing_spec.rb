6df658b82cf64c650d3c2012ae9b2f8524e6417a
qb0C80aE <qb0C80aE@gmail.com>
2017-05-10 02:20:12 +0000

    Add site show function

--- spec/routing/sites_routing_spec.rb
+++ spec/routing/sites_routing_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe SitesController, type: :routing do
       expect(:get => "/sites").to route_to("sites#index")
     end
 
+    it "routes to #show" do
+      expect(:get => "/sites/1").to route_to("sites#show", :id => "1")
+    end
+
   end
 end
