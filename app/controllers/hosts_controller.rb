4d7636e1bf45e3c33d18c240732257668a1f0baa
qb0C80aE <qb0C80aE@gmail.com>
2017-05-24 21:06:11 +0900

    Extract run_net_tester from routing handler

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,38 +1,38 @@
 class HostsController < ApplicationController
+
   # GET /hosts
   def index
-    @hosts = Phut::Netns.all
-    render json: @hosts
+    hosts = Phut::Netns.all
+    render json: hosts, status: :ok
   end
 
   # GET /hosts/name
   def show
-    @host = Phut::Netns.find_by(name: params[:id])
-    render json: @host
+    result = Phut::Netns.find_by(name: params[:name])
+    code = :ok
+    if result.nil? then
+      result = {error: "no such host: #{params[:name]}"}
+      code = :not_found
+    end
+    render json: result, status: code
   end
 
   # PUT /hosts/name
   def update
-    unless NetTester.running? then
-      NetTester.log_dir = File.join(Aruba.config.working_directory, 'log')
-      NetTester.pid_dir = File.join(Aruba.config.working_directory, 'pids')
-      NetTester.socket_dir = File.join(Aruba.config.working_directory, 'sockets')
-      device = ENV['DEVICE'] || 'eth1'
-      dpid = ENV['DPID'].try(&:hex) || 0x123
-      NetTester.run(network_device: device, physical_switch_dpid: dpid)
-      sleep 2
+    run_result = run_net_tester
+    unless run_result.nil? then
+      result = {error: run_result}
+      render json: result, status: :internal_server_error
+    else
+      host = Phut::Netns.find_by(name: params[:name])
+      if host.nil? then
+        netns_params = host_params.permit!
+        netns_params = netns_params.to_h.symbolize_keys
+        netns_params[:name] = params[:name]
+        host = NetTester::Netns.new(netns_params)
+      end
+      render json: host, status: :ok
     end
-
-    netns_params = host_params.permit!
-    netns_params = netns_params.to_h.symbolize_keys
-    netns_params[:name] = params[:name]
-    @host = NetTester::Netns.new(netns_params)
-    render json: @host
-  end
-
-  # DELETE /hosts/name
-  def destroy
-    Phut::Netns.find_by(name: params[:id]).stop
   end
 
   private
@@ -40,4 +40,19 @@ class HostsController < ApplicationController
     def host_params
       params.fetch(:host, {})
     end
+
+   # Run NetTester if that's not running.
+   def run_net_tester
+    return nil if NetTester.running?
+    FileUtils.mkdir_p(NetTester.log_dir)
+    FileUtils.mkdir_p(NetTester.pid_dir)
+    FileUtils.mkdir_p(NetTester.socket_dir)
+    device = ENV['DEVICE'] || 'eth1'
+    dpid = ENV['DPID'].try(&:hex) || 0x123
+    NetTester.run(network_device: device, physical_switch_dpid: dpid)
+    sleep 2
+    nil
+   rescue => e
+    e
+   end
 end
