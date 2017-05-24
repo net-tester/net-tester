7123c7376518c43bf326d080f0e137178689faaa
qb0C80aE <qb0C80aE@gmail.com>
2017-05-24 20:59:46 +0900

    Implement default error routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+  rescue_from Exception, with: :_render_500
+  rescue_from ActionController::RoutingError, with: :_render_404
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  private
+
+  def _render_404(e = nil)
+    render json: {error: 'resource not found'}, status: :not_found
+  end
+
+  def _render_500(e = nil)
+    render json: {error: 'internal server error'}, status: :internal_server_error
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resource :sites, :only => [:show, :destroy]
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
+  put '*not_found' => 'application#routing_error'
+  delete '*not_found' => 'application#routing_error'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
