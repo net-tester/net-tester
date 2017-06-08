5fefd8a19ebae7c8e00b4635b813376d1c242792
qb0C80aE <qb0C80aE@gmail.com>
2017-06-08 15:26:13 +0900

    Fix whitelisted_ips

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_console.whitelisted_ips = ENV['WHITELISTED_IPS'] || '0.0.0.0/0'
 end
