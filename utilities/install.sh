b3e23f6e969261a13016fee636931fa689fe8772
qb0C80aE <qb0C80aE@gmail.com>
2017-08-10 17:55:02 +0900

    Fix install.sh to copy the service file

--- utilities/install.sh
+++ utilities/install.sh
@@ -9,7 +9,7 @@ sudo -E -u net-tester git config --global url."https://".insteadOf "git://"
 sudo -E -u net-tester git checkout feature/api-merge
 sudo -E -u net-tester bundle install --path=vendor/bundle
 cp /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester
-ln -s /opt/net-tester/utilities/net-tester.service /etc/systemd/system/net-tester.service
+cp /opt/net-tester/utilities/net-tester.service /etc/systemd/system/net-tester.service
 systemctl daemon-reload
 systemctl enable net-tester
 systemctl start net-tester
