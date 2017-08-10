3d365bfabb2f66a9901f8c2b278f2d928264f200
qb0C80aE <qb0C80aE@gmail.com>
2017-08-10 17:12:28 +0900

    Fix install.sh to give sudo when it uses git, and fix paths to absolute at ln

--- utilities/install.sh
+++ utilities/install.sh
@@ -4,11 +4,11 @@ cd /opt
 git clone https://github.com/net-tester/net-tester.git
 chown -R net-tester:net-tester net-tester
 cd net-tester
-git config --local url."https://".insteadOf "git://"
-git checkout feature/api-merge
+sudo -u net-tester git config --local url."https://".insteadOf "git://"
+sudo -u net-tester git checkout feature/api-merge
 sudo -u net-tester bundle install --path=vendor/bundle
-ln -s utilities/net-tester.sudoers /etc/sudoers.d/net-tester.sudoers
-ln -s utilities/net-tester.service /etc/systemd/system/net-tester.service
+ln -s /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester.sudoers
+ln -s /opt/net-tester/utilities/net-tester.service /etc/systemd/system/net-tester.service
 systemctl daemon-reload
 systemctl enable net-tester
 systemctl start net-tester
