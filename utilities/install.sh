1b6af26746b9d90cec7c122928ae089e20146727
qb0C80aE <qb0C80aE@gmail.com>
2017-08-10 17:01:07 +0900

    Fix install.sh to execute bundle install as net-tester user

--- utilities/install.sh
+++ utilities/install.sh
@@ -5,7 +5,7 @@ git clone https://github.com/net-tester/net-tester.git
 chown -R net-tester:net-tester net-tester
 cd net-tester
 git checkout feature/api-merge
-bundle install --path=vendor/bundle
+sudo -u net-tester bundle install --path=vendor/bundle
 cp utilities/net-tester.sudoers /etc/sudoers.d/
 cp utilities/net-tester.service /etc/systemd/system/
 systemctl daemon-reload
