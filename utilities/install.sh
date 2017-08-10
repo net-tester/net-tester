2e361182593f4b511affc84d94377f198da58b69
qb0C80aE <qb0C80aE@gmail.com>
2017-08-10 17:39:22 +0900

    Fix sudoers

--- utilities/install.sh
+++ utilities/install.sh
@@ -1,16 +1,16 @@
 sudo sh <<EOF
-adduser --shell /usr/sbin/nologin net-tester
+adduser --home /home/net-tester --shell /usr/sbin/nologin net-tester
 cd /opt
 git clone https://github.com/net-tester/net-tester.git
 chown -R net-tester:net-tester net-tester
 cd net-tester
-sudo -u net-tester git config --local url."https://".insteadOf "git://"
-sudo -u net-tester git checkout feature/api-merge
-sudo -u net-tester bundle install --path=vendor/bundle
-ln -s /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester.sudoers
+export HOME=/home/net-tester
+sudo -E -u net-tester git config --global url."https://".insteadOf "git://"
+sudo -E -u net-tester git checkout feature/api-merge
+sudo -E -u net-tester bundle install --path=vendor/bundle
+cp /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester
 ln -s /opt/net-tester/utilities/net-tester.service /etc/systemd/system/net-tester.service
 systemctl daemon-reload
 systemctl enable net-tester
 systemctl start net-tester
 EOF
-
