48cf7a0910f61f4ce0bc41626214313d056fc8a4
qb0C80aE <qb0C80aE@gmail.com>
2017-08-17 14:42:20 +0900

    Fix install.sh to use no-create-home

--- utilities/install.sh
+++ utilities/install.sh
@@ -1,11 +1,12 @@
 sudo sh <<EOF
-adduser --shell /usr/sbin/nologin net-tester
+adduser --no-create-home --shell /usr/sbin/nologin net-tester
 cd /opt
 git clone https://github.com/net-tester/net-tester.git
 chown -R net-tester:net-tester net-tester
 cd net-tester
-sudo -u net-tester git checkout feature/api-merge
-sudo -u net-tester bundle install --path=vendor/bundle
+export HOME=/opt/net-tester/log
+sudo -E -u net-tester git checkout feature/api-merge
+sudo -E -u net-tester bundle install --path=vendor/bundle
 cp /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester
 cp /opt/net-tester/utilities/net-tester.service /lib/systemd/system/net-tester.service
 systemctl daemon-reload
