e55a049dcd7240ea7b0309f5a22c0c6ef9e65863
qb0C80aE <qb0C80aE@gmail.com>
2017-08-17 14:16:13 +0900

    Fix installer.sh not to use git insteadof option

--- utilities/install.sh
+++ utilities/install.sh
@@ -1,13 +1,11 @@
 sudo sh <<EOF
-adduser --home /home/net-tester --shell /usr/sbin/nologin net-tester
+adduser --shell /usr/sbin/nologin net-tester
 cd /opt
 git clone https://github.com/net-tester/net-tester.git
 chown -R net-tester:net-tester net-tester
 cd net-tester
-export HOME=/home/net-tester
-sudo -E -u net-tester git config --global url."https://".insteadOf "git://"
-sudo -E -u net-tester git checkout feature/api-merge
-sudo -E -u net-tester bundle install --path=vendor/bundle
+sudo -u net-tester git checkout feature/api-merge
+sudo -u net-tester bundle install --path=vendor/bundle
 cp /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester
 cp /opt/net-tester/utilities/net-tester.service /lib/systemd/system/net-tester.service
 systemctl daemon-reload
