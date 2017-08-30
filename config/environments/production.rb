ebfede1c8eb40e3c189860388c38a62492a21fda
qb0C80aE <qb0C80aE@gmail.com>
2017-08-30 08:41:04 +0000

    Remove unnecessary sqlite3 dependency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,5 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
   config.web_console.whitelisted_ips = ENV['WHITELISTED_IPS'] || '0.0.0.0/0'
 end
