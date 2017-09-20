851d651bf3b7999bcbe11b16094856c68091d4f2
qb0C80aE <qb0C80aE@gmail.com>
2017-09-20 03:21:06 +0000

    Change the checkout branch to develop

--- utilities/install.sh
+++ utilities/install.sh
@@ -5,7 +5,6 @@ git clone https://github.com/net-tester/net-tester.git
 chown -R net-tester:net-tester net-tester
 cd net-tester
 export HOME=/opt/net-tester/log
-sudo -E -u net-tester git checkout feature/api-merge
 sudo -E -u net-tester bundle install --path=vendor/bundle
 cp /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester
 cp /opt/net-tester/utilities/net-tester.service /lib/systemd/system/net-tester.service
