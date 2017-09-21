036d16335699fd83ea52dbc8c828c9c5355cd8b1
qb0C80aE <qb0C80aE@gmail.com>
2017-09-21 13:07:12 +0900

    Move utilities to script

--- script/install.sh
+++ script/install.sh
@@ -6,8 +6,8 @@ chown -R net-tester:net-tester net-tester
 cd net-tester
 export HOME=/opt/net-tester/log
 sudo -E -u net-tester bundle install --path=vendor/bundle
-cp /opt/net-tester/utilities/net-tester.sudoers /etc/sudoers.d/net-tester
-cp /opt/net-tester/utilities/net-tester.service /lib/systemd/system/net-tester.service
+cp /opt/net-tester/script/net-tester.sudoers /etc/sudoers.d/net-tester
+cp /opt/net-tester/script/net-tester.service /lib/systemd/system/net-tester.service
 systemctl daemon-reload
 systemctl enable net-tester
 systemctl start net-tester
